a0342f211eeef2de50f321aeb2a7ebd8dd433a88
hirotane <htomomi1019@gmail.com>
2021-07-22 14:24:53 +0900

    focus title

--- utils.js
+++ utils.js
@@ -1,8 +1,6 @@
 // Globals
 var shortcuts = {
     defaultOptions: {
-        // Next = J; Previous = K [WARNING: Conflicts with activateSearch. This takes precedence.]
-        // navigateWithJK: false
         navigateWithJK: true
     },
     focusIndex: -1,
@@ -16,16 +14,14 @@ var shortcuts = {
     },
     focusResult: function(offset) {
         var results = this.getVisibleResults();
-        // console.log(results);
         this.focusIndex += offset;
         this.focusIndex = Math.min(this.focusIndex, results.length - 1);
         this.focusIndex = Math.max(this.focusIndex, 0);
         var target = results[this.focusIndex];
         var target_txt = target.querySelector("h2 > a");
-        // var r = this.focusIndex + 1 + "/" + results.length + " ▶";
         console.log(target);
-        console.log(target_txt.innerHTML);
-        return target_txt.style.backgroundColor = 'red'
+        console.log(target_txt);
+        target_txt.focus();
     }
 
 };
